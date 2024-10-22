87d224d63a2fc02ffc446ba9f0e4aa7787363ee8
Jason Holland <jlholland@bert.appdevjoy.dev>
2024-10-22 15:51:36 -0600

    added correlation id

--- src/main/java/net/appdevjoy/testRestJms/component/JmsProcessor.java
+++ src/main/java/net/appdevjoy/testRestJms/component/JmsProcessor.java
@@ -2,11 +2,17 @@ package net.appdevjoy.testRestJms.component;
 
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,16 +27,52 @@ public class JmsProcessor {
     }
 
     @JmsListener(destination = "senQ")
-    public void receiveAndReturn(String msg) {
+    public void receiveAndReturn(Message msg) {
         log.info("Received Msg: " + msg);
-            int randInt = rand.nextInt(10);
-            try {
-                TimeUnit.SECONDS.sleep(randInt);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        int randInt = rand.nextInt(10);
+        try {
+            TimeUnit.SECONDS.sleep(randInt);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        String retMsg;
+        try {
+            String body = null;
+             if (msg instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) msg;
+                body = textMessage.getText();
+
+                // Process the message body
+                System.out.println("Received message: " + body);
+
+            } else {
+                // Handle other message types if needed
+                System.out.println("Received a non-text message");
             }
-        String retMsg = "JMS Received Msg -- " + msg + " jmsMsgCount: " + counter.getAndIncrement() + " slept: " + randInt;
-        jmsTemplate.convertAndSend("retQ", retMsg);
+            log.info("msgBody: " + body);
+            retMsg = "JMS Received Msg -- CorrelationId: " + msg.getJMSCorrelationID() + " body: " + body+ " jmsMsgCount: " + counter.getAndIncrement()
+                    + " slept: " + randInt;
+            jmsTemplate.convertAndSend("retQ", retMsg, new CorrelationIdPostProcessor(msg.getJMSCorrelationID()));
+        } catch (JMSException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private class CorrelationIdPostProcessor implements MessagePostProcessor {
+        private final String correlationId;
+
+        public CorrelationIdPostProcessor(final String correlationId) {
+            this.correlationId = correlationId;
+        }
+
+        @Override
+        public Message postProcessMessage(final Message msg)
+                throws JMSException {
+            msg.setJMSCorrelationID(correlationId);
+            return msg;
+        }
     }
 }
--- src/main/java/net/appdevjoy/testRestJms/controller/HelloRestController.java
+++ src/main/java/net/appdevjoy/testRestJms/controller/HelloRestController.java
@@ -3,14 +3,18 @@ package net.appdevjoy.testRestJms.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
+import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -19,7 +23,7 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/")
 public class HelloRestController {
     private JmsTemplate jmsTemplate;
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicInteger counter = new AtomicInteger();
     private final Random rand = new Random();
 
     public HelloRestController(JmsTemplate jmsTemplate) {
@@ -28,7 +32,8 @@ public class HelloRestController {
 
     @GetMapping("/hello") 
     public ResponseEntity<String> hello() {
-        log.info("hello called " + counter.get());
+        int tmpCounter = counter.getAndIncrement();
+        log.info("hello called " + tmpCounter);
             int randInt = rand.nextInt(10);
             try {
                 TimeUnit.SECONDS.sleep(randInt);
@@ -36,9 +41,26 @@ public class HelloRestController {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            String jmsMsg = "Rest Controller slept: " +randInt+ " sent from: " + counter.getAndIncrement(); 
-            jmsTemplate.convertAndSend("senQ", jmsMsg);;
-            String retMsg = (String) jmsTemplate.receiveAndConvert("retQ");
-            return new ResponseEntity<String>("Returning from restCall, from jms[" + retMsg + "]\n", HttpStatus.OK);
+            String jmsMsg = "Rest Controller sent from: " + tmpCounter + " slept: " +randInt; 
+           
+            jmsTemplate.convertAndSend("senQ", jmsMsg, new CorrelationIdPostProcessor(String.valueOf(tmpCounter)));
+            String retMsg = (String) jmsTemplate.receiveSelectedAndConvert("retQ","JMSCorrelationID = '" + tmpCounter + "'");
+            // String retMsg = (String) jmsTemplate.receiveAndConvert("retQ");
+            return new ResponseEntity<String>("Returning from restCall thisCounter: "+tmpCounter+", from jms[" + retMsg + "]\n", HttpStatus.OK);
+    }
+
+    private class CorrelationIdPostProcessor implements MessagePostProcessor {
+        private final String correlationId;
+
+        public CorrelationIdPostProcessor(final String correlationId) {
+            this.correlationId = correlationId;
+        }
+
+        @Override
+        public Message postProcessMessage(final Message msg)
+                throws JMSException {
+            msg.setJMSCorrelationID(correlationId);
+            return msg;
+        }
     }
 }
